d95a2045d93a2102445a18c9913047e85b8f32c2
user <jeanriffel1@gmai.com>
2026-02-12 07:14:51 -0300

    feat: implementing clock on Account

--- src/application/uses-cases/account/create-account-use-case.ts
+++ src/application/uses-cases/account/create-account-use-case.ts
@@ -1,18 +1,21 @@
 import { Account } from "../../../domain/entities/account"
 import { AccountId } from "../../../domain/value-objects/account-id"
 import { AccountStatus } from "../../../domain/value-objects/account-status-value-object"
+import { SystemClock } from "../../../infra/time/system-clock"
 
 export class CreateAccountUseCase {
 
   constructor(){}
 
   async execute(): Promise<boolean>{
+    const accountId =  AccountId.generate()
     const accountStatus = new AccountStatus(1)
+    const createdAt = new SystemClock().now()
 
     const account = new Account(
-      AccountId.generate(),
+      accountId,
       accountStatus,
-      new Date()
+      createdAt
     )
     
     return account.isValid()
